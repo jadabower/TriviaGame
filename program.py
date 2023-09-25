21849452e3b1e13b07148111b0d8054f5a6b159c
Clark Namauu <Clarknamauu@gmail.com>
2023-09-25 16:00:25 -0600

    open window

--- program.py
+++ program.py
@@ -1,1 +1,12 @@
+import arcade
+
+arcade.open_window(600, 600, "Trivia")
+arcade.set_background_color(arcade.color.WHITE)
+
+arcade.start_render()
+
+arcade.finish_render()
+
+arcade.run()
+
 print("Hello World!")
