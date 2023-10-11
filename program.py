d20832d2735ccd3766a7f7196ffac41ef68e9842
Spencer Damiano <86030230+Spencer-Damiano@users.noreply.github.com>
2023-10-11 10:45:03 -0600

    menu button works
    we need to find assets for the other buttons and such.

--- program.py
+++ program.py
@@ -1,17 +1,49 @@
 import pygame
 from sys import exit
+import button
 
-# Variables
+# CONSTANTS
+SCREEN_WIDTH = 800
+SCREEN_HEIGHT = 600
+
+# Variables 
 pygame.init()
-screen = pygame.display.set_mode((800,400))
-pygame.display.set_caption('Trivia Game')
-game_active = False
 
-test_surf = pygame.image.load('graphics/start.png').convert_alpha()
-test_rect = test_surf.get_rect(center = (400,200))
+screen = pygame.display.set_mode((SCREEN_WIDTH,SCREEN_HEIGHT))
+
+pygame.display.set_caption('Trivia Game')
+
+
+# test_surf = pygame.image.load('graphics/start.png').convert_alpha()
+# test_rect = test_surf.get_rect(center = (400,200))
+
+# Game Variables
+game_active = False
+menu_active = True
+
+# create button images
+play_img = pygame.image.load('graphics/play_button.png').convert_alpha()
+
+# create button instances
+play_button = button.Button(SCREEN_WIDTH*.1 , SCREEN_HEIGHT*.10, play_img, .25)
+
+
+
+# define fonts
+font = pygame.font.SysFont('Futura',30)
+
+# define colors
+WHITE = (255,255,255)
+TEAL = (6, 81, 67)
+
+# define functions
+def draw_text(text, font, text_col, x, y):
+    img = font.render(text, True, text_col)
+    screen.blit(img,(x,y))
 
 # Game loop
 while True:
+
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
@@ -20,5 +52,12 @@ while True:
         pygame.quit()
         exit()
     else:
-        screen.fill('#065143')
-        screen.blit(test_surf,test_rect)
+        screen.fill((TEAL))
+        # screen.blit(test_surf,test_rect)
+        if menu_active:
+            if play_button.draw(screen):
+                menu_active = False
+                print("Menu Button Works")
+        else: 
+            draw_text("hello word", font, WHITE, 160, 250)
+        pygame.display.update()
