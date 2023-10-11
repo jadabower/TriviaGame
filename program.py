a08661cc81822c4c35a8f46ccb2ad5c33b714e6c
Jada Bower <131042500+jadabower@users.noreply.github.com>
2023-10-10 20:19:19 -0600

    switched to pygame

--- program.py
+++ program.py
@@ -1,12 +1,24 @@
-import arcade
+import pygame
+from sys import exit
 
-arcade.open_window(600, 600, "Trivia")
-arcade.set_background_color(arcade.color.WHITE)
+# Variables
+pygame.init()
+screen = pygame.display.set_mode((800,400))
+pygame.display.set_caption('Trivia Game')
+game_active = False
 
-arcade.start_render()
+test_surf = pygame.image.load('graphics/start.png').convert_alpha()
+test_rect = test_surf.get_rect(center = (400,200))
 
-arcade.finish_render()
-
-arcade.run()
-
-print("Hello World!")
+# Game loop
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            exit()
+    if game_active:
+        pygame.quit()
+        exit()
+    else:
+        screen.fill('#065143')
+        screen.blit(test_surf,test_rect)
